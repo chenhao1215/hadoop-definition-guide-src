89a65aef52882515020d0ec2b45ed5b808c2706f
Lucifer <Lucifer@ghost-PC>
2012-02-18 18:38:44 +0800

    first

---
+++ hadooplearn/src/first/MaxTemperatureMapper.java
@@ -0,0 +1,28 @@
+package first;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class MaxTemperatureMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>{
+
+	@Override
+	public void map(LongWritable key, Text value,
+			OutputCollector<Text, IntWritable> output, Reporter reporter)
+			throws IOException {
+		String line=value.toString();
+		String[] lineDate=line.split(",");
+		String year=lineDate[0];
+		
+		int airTemperature=Integer.parseInt(lineDate[1]);
+		
+		output.collect(new Text(year), new IntWritable(airTemperature));
+	}
+
+}
