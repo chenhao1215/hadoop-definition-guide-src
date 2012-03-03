b9b5c66ddcb13a9ed7eb7af6aa35ea68d19dc856
lotusk <lotus@qq.com>
2012-03-03 23:54:06 +0800

    Signed-off-by: lotusk <lotus@qq.com>

---
+++ hadooplearn/src/hdfs/ShowFileStatusTest.java
@@ -0,0 +1,71 @@
+package hdfs;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.el.LessThanOrEqualsOperator;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+import static org.hamcrest.Matcher.*;
+
+public class ShowFileStatusTest {
+	private MiniDFSCluster cluster; //use an in-process HDFS cluster for testing
+	private FileSystem fs;
+	
+	@Before
+	public void setUp() throws IOException{
+		Configuration conf=new Configuration();
+		if (System.getProperty("test.build.data")==null) {
+			System.setProperty("test.build.data", "/tmp");
+		}
+		cluster =new MiniDFSCluster(conf,1,true,null);
+		
+		fs=cluster.getFileSystem();
+		
+		OutputStream out=fs.create(new Path("/dir/file"));
+		out.write("content".getBytes("UTF-8"));
+		out.close();
+	}
+	
+	@After
+	public void tearDown() throws IOException{
+		if (fs!=null) {
+			fs.close();
+		}
+		if (cluster!=null) {
+			cluster.shutdown();
+		}
+	}
+	
+	@Test(expected=FileNotFoundException.class)
+	public void throwsFileNotFoundForNonExistentFile() throws IOException{
+		fs.getFileStatus(new Path("no-such-file"));
+	}
+	
+	@Test
+	public void fileStatusForFile() throws IOException{
+		Path file=new Path("/dir/file");
+		FileStatus stat=fs.getFileStatus(file);
+		
+		assertThat(stat.getPath().toUri().getPath(), is("/dir/file"));
+		System.out.println(stat.getPath().toUri().getPath());
+		System.out.println(stat.isDirectory());
+		assertThat(stat.isDirectory(), is(false));
+		assertThat(stat.getLen(), is(7l));
+		assertTrue( System.currentTimeMillis()>=stat.getModificationTime());
+		assertThat(stat.getReplication(), is((short)1));
+		assertThat(stat.getBlockSize(), is((64*1024*1024l)));
+		assertThat(stat.getOwner(), is("Lucifer"));
+		assertThat(stat.getGroup(), is("supergroup"));
+		assertThat(stat.getPermission().toString(), is("rw-r--r--"));
+	}
+}
